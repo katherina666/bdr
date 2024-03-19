351c6f46be349f28cd71622e713c06167372a416
katherina666 <katarzyna.keller@gmail.com>
2024-03-19 18:04:53 +0100

    zadanie 1

---
+++ tests/basic.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://demo.evershop.io/');
+
+  //css
+  //await page.locator('css=#app > div > main > div.mt-15 > div > div:nth-child(2) > a > span').click()
+
+  //xpath
+
+//playwright
+  await page.getByRole('link', { name: 'Shop women' }).click();
+
+});
---
+++ tests/basicOperation.ts
@@ -0,0 +1,17 @@
+//console.log('hello world')
+
+const number1: number = 10 // stała, nie możemy jej nadpisać
+let number2: number = 20 // zmienna
+
+console.log('numer: ' + number2)
+number2 = 30
+console.log('numer: ' + number2)
+let text1: string = '30'
+
+// dodawanie
+let sum = number2 + text1
+console.log('suma: ' + sum)
+
+// odejmowanie
+let odejmowanie = number2 - number1
+console.log('suma: ' + odejmowanie)
